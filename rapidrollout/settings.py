20b1ddd5808c9247c05d9ec8dd8a56933ee804f4
David G.B. <davidgballester@outlook.es>
2025-05-22 10:32:53 +0200

    Added missing static and media roots

--- rapidrollout/settings.py
+++ rapidrollout/settings.py
@@ -138,3 +138,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
